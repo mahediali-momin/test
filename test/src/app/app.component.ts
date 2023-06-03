eb115386245b80897ab38c26bdd110087bb9977f
mahediali-momin <mahedialis.momin@gmail.com>
2023-06-03 19:56:07 +0530

    implementation of the pwa, check the indexdb

--- test/src/app/app.component.ts
+++ test/src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test';
+  update: boolean = false
+
+  dbPromise = window.indexedDB.open('test-db1', 1);
+
+  constructor(
+    private updates: SwUpdate
+  ) {
+    this.updates.available.subscribe(event => {
+      console.log("event: ", event)
+      // this.update = true                     
+      updates.activateUpdate().then(() => {
+        console.log("yes new  update is ready for the serve")
+
+        window.location.reload();
+
+      })
+    })
+  }
+
+  ngOnInit(): void {
+    this.checkdb()
+  }
+
+  checkdb() {
+    if (!('indexedDB' in window)) {
+      console.log("This browser doesn't support IndexedDB");
+      return;
+    } else {
+      console.log("This browser  support IndexedDB");
+
+    }
+  }
 }
